5ac9c7ac5c5bd1fa571bd31afa8bae0b6d70d26c
Ismael <isma907@gmail.com>
2026-03-22 13:39:11 -0300

    unified ui

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -23,7 +23,7 @@ export const ProductCard = ({ product }: { product: Product }) => {
           </h3>
           {/* <p className="mt-1 text-sm text-gray-500">{product.category}</p> */}
         </div>
-        <p className="text-sm font-medium text-gray-900">{product.price}</p>
+        <p className="text-sm font-medium text-gray-900">${product.price}</p>
       </div>
     </div>
   );
--- src/pages/product/Product.tsx
+++ src/pages/product/Product.tsx
@@ -27,7 +27,7 @@ const Product = () => {
 
   return (
     <>
-      <div className="pt-6">
+      <div className="pt-16">
         <nav aria-label="Breadcrumb">
           <ol
             role="list"
@@ -137,8 +137,8 @@ const Product = () => {
           </div>
         </div>
 
-        <div className="w-full bg-gray-100 py-6">
-          <div className="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl">
+        <div className="w-full bg-gray-100">
+          <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
             <RelatedProducts productId={productId!} />
           </div>
         </div>
