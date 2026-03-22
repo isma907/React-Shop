973212247e05a2ee5345f7d1da03126eae33fe38
Ismael <isma907@gmail.com>
2026-03-22 15:27:10 -0300

    unified ui

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -27,10 +27,13 @@ function HomeProducts() {
       </div>
     );
   return (
-    <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-      {productsQuery.data?.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
-    </div>
+    <>
+      <h2 className="text-2xl font-bold tracking-tight text-gray-900">Products</h2>
+      <div className="mt-6 grid grid-cols-2 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+        {productsQuery.data?.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </>
   );
 }
